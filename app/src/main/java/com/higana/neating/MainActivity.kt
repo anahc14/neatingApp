d8ecd7857a6d1f68fce61c729e11f332f56eb163
Ana Higueras <anahc96@hotmail.com>
2020-05-24 16:24:01 +0200

    Adding whole project

---
+++ app/src/main/java/com/higana/neating/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.higana.neating
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.annotation.Nullable
+
+class MainActivity : Activity() {
+
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/higana/neating/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.higana.neating
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
